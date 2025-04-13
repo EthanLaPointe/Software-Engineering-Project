4b9885ecfcc5492dc985e86e0be34efcb1389596
Tucha3872 <167715318+Tucha3872@users.noreply.github.com>
2025-04-13 17:35:29 -0400

    JDBC Test
    Tested a connection within Java to a test database. Made sure the database was query-able and did some insert statements.

---
+++ JDBC Test/src/main/java/test.java
@@ -0,0 +1,50 @@
+import java.sql.*;
+
+public class test {
+
+    Connection connection;
+    public test(){
+
+    }
+    public static void main(String[] args) {
+        test test = new test();
+        test.createConnection();
+    }
+
+    void createConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection
+                    ("jdbc:mysql://localhost:3306/test_gamed", "root", "_____");
+            System.out.println("Successfully connected");
+            Statement statement = connection.createStatement();
+
+            //Gets all columns from the Game Table and displays them
+            ResultSet resultSet = statement.executeQuery("Select * from Game");
+            while (resultSet.next()){
+                String result = "";
+                for(int i = 1; i <=3; i++) {
+                    result += resultSet.getString(i) + " ";
+                }
+                System.out.println(result);
+            }
+            System.out.println("\n");
+
+            //Insert Statement
+            /*String insertString = "insert into Game (title, relDate) values ('Call of Duty', '2022-01-05')";
+            statement.execute(insertString);*/
+
+            //Gets all columns and displays based on what the column name/index is
+            resultSet = statement.executeQuery("Select * from Game");
+            while (resultSet.next()){
+                String name = resultSet.getString("title");
+                System.out.println(name);
+            }
+            statement.close();
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
