a938001b958437f7a1869332c4b3afff99da67b3
EthanLaPointe <e.lapointe@comcast.net>
2025-04-16 19:45:41 -0400

    Game list retrieval using API wrapper

---
+++ IGDBConnection/gamedapi/src/main/java/gamed/APIHandler.java
@@ -0,0 +1,14 @@
+package gamed;
+
+import com.api.igdb.request.TwitchAuthenticator;
+import com.api.igdb.utils.TwitchToken;
+
+public class APIHandler 
+{
+    
+
+    public APIHandler() 
+    {
+        // Constructor logic if needed
+    }
+}
