b7a0d1e4efbf9411cc4bc3e7d75e37276310ccaf
dvasil23 <121838951+dvasil23@users.noreply.github.com>
2025-04-27 22:49:40 -0400

    app is runnable
    added the bycrypt dependency to all pom files (dont think it was needed). added a line in module info that manually opens the gamedtestproject package. need to use the register controller class within the fxml to hash the account information in the database.

--- GamedGUI/src/main/java/module-info.java
+++ GamedGUI/src/main/java/module-info.java
@@ -2,7 +2,8 @@ module gamed.gamedtestproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql; // for database stuff
-    requires bcrypt;
+    requires bcrypt; // Removed as bcrypt is not a valid module
+    opens gamed.gamedtestproject to javafx.fxml;
     exports gamed.gamedtestproject;
 
 }
