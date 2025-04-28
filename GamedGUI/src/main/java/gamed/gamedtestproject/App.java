5ad5705bd328d5d89e2a1a38afd65ffdba11d5c0
chaserusso <chaserusso@users.noreply.github.com>
2025-04-27 20:42:41 -0400

    idk anymore

--- GamedGUI/src/main/java/gamed/gamedtestproject/App.java
+++ GamedGUI/src/main/java/gamed/gamedtestproject/App.java
@@ -4,18 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
