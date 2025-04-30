d9e727ef9d3f1701b9c0502279b82b866a34b3df
dvasil23 <121838951+dvasil23@users.noreply.github.com>
2025-04-30 18:13:20 -0400

    fixed bcrypt error
    added requries bcrypt to modules info file

--- GamedGUI/src/main/java/module-info.java
+++ GamedGUI/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module gamed.gamedtestproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql; // for database stuff
+    requires bcrypt;
     opens gamed.gamedtestproject to javafx.fxml;
     exports gamed.gamedtestproject;
 
