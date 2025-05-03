b115e66dcec631bab22fa36b5eec189ec4d54d15
chaserusso <chaserusso@users.noreply.github.com>
2025-05-03 18:31:21 -0400

    new db

---
+++ gamed_db.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS Gamed_db;
+CREATE DATABASE Gamed_db;
+USE Gamed_db;
+
+CREATE TABLE Accounts
+(
+	account_id int auto_increment primary key,
+    username VARCHAR(128) NOT NULL UNIQUE,
+    password VARCHAR(128) NOT NULL,
+    dateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE WishLists
+(
+    account_id INT,
+    game_id INT,
+	FOREIGN KEY (account_id) REFERENCES Accounts(account_id) ON DELETE CASCADE
+
+);
+
+CREATE TABLE FavGames
+(
+    account_id INT,
+    game_id INT,
+	FOREIGN KEY (account_id) REFERENCES Accounts(account_id) ON DELETE CASCADE
+	
+);
+
+CREATE TABLE Reviews
+(
+    review_id INT auto_increment primary key, 
+    game_id INT,
+	account_id INT,
+    rating INT NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id) ON DELETE CASCADE
+);
